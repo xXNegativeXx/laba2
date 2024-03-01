edd5c7299f6749ca617f40a0b627627c6b8b8ad0
xXNegativeXx <160238937+xXNegativeXx@users.noreply.github.com>
2024-03-01 17:30:29 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,49 +1,13 @@
-import time
-import uvicorn
-import pika
-from fastapi import FastAPI
-import logging
+from flask import Flask
 
-logging.basicConfig(level='INFO', format="%(levelname)s - %(asctime)s - %(message)s")
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
+app = Flask(_name_)
 
-try:
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
-    channel = connection.channel()
-    logger.info('Соединение с брокером сообщений установлено')
-    channel.queue_declare(queue='hello')
+@app.route('/')
+def home():
+    return 'It is working!'
 
-    number = 0
-    while number < 10:
-        time.sleep(5)
-        channel.basic_publish(exchange='', routing_key='hello', body=f'Hi World! {number}')
-        # print(f" [{number}] Sent message")
-        logger.info(f'Сообщение {number} отправлено в очередь')
-        number += 1
-    connection.close()
-except:
-    logger.critical('Соединение с брокером не установлено')
+def main():
+    app.run(host='0.0.0.0', port=89)
 
-# app = FastAPI(title='Service1')
-
-
-# @app.get('/service1')
-# def get_service1():
-#     connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq'))
-#     channel = connection.channel()
-#     channel.queue_declare(queue='hello')
-#
-#     number = 0
-#     while number < 50:
-#         time.sleep(5)
-#         channel.basic_publish(exchange='', routing_key='hello', body='Hi World!')
-#         print(f" {number} Sent message")
-#         number += 1
-#     connection.close()
-#     return {"Service 1"}
-#
-#
-# if __name__ == '__main__':
-#     uvicorn.run("main:app", host='0.0.0.0', port=7040, reload=True)
-
+if __name__=='__main__':
+    main()
